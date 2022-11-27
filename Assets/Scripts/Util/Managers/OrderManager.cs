416a66dd60af03ca3462d0bb7dcf070726ccd78b
Jan Maceček <xmacece1@fi.muni.cz>
2022-11-27 14:32:04 +0100

    All current items and potions are prefabs

---
+++ Assets/Scripts/Util/Managers/OrderManager.cs
@@ -0,0 +1,23 @@
+using PotionsPlease.InGame;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PotionsPlease.Util.Managers
+{
+    public class OrderManager : MonoBehaviour
+    {
+        [SerializeField] public List<PotionObject> potions;
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
